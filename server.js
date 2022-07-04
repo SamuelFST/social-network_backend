329691b3bbbeff2c7e690854c50fa86b26c30200
SamuelFST <guti.samuel2002@gmail.com>
2022-07-04 11:57:50 -0300

    refactor: getting the routes from the routes file

--- server.js
+++ server.js
@@ -7,55 +7,11 @@ const app = express();
 const port = process.env.API_PORT;
 
 import bodyParser from 'body-parser';
-
-const router = express.Router();
+import routes from './src/routes/routes';
 
 app.use(bodyParser.json());
+app.use(routes);
 
-router.route('/')
-  .all((req, res, next) => {
-    console.log(`Request from : ${req.originalUrl}`);
-    console.log(`Request type : ${req.method}`);
-    console.log(`Request params : ${req.params}`);
-    next();
-  })
-  .get((req, res) => {
-    console.log(req.params);
-    res.send(JSON.stringify([]));
-    res.status(200);
-    res.end();
-  })
-  .post((req, res) => {
-    console.log(JSON.stringify(req.body));
-    res.status(201);
-    res.end();
-  });
-
-router
-  .param('id', (req, res, next, id) => {
-    console.log(`Request from : ${req.params}`);
-    console.log(`Request type : ${req.method}`);
-    console.log(`Request id : ${id}`);
-    next();
-  })
-  .route('/:id')
-  .get((req, res) => {
-    console.log(req.params);
-    res.send(JSON.stringify({}));
-    res.status(200);
-    res.end();
-  })
-  .put((req, res) => {
-    console.log(req.body);
-    res.status(203);
-    res.end();
-  })
-  .delete((req, res) => {
-    res.status(203);
-    res.end();
-  });
-
-app.use('/posts', router);
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
