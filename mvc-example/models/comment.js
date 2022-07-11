831e3e5f58c65489e8fa017c7a72ca095751e1b7
SamuelFST <guti.samuel2002@gmail.com>
2022-07-11 20:24:34 -0300

    feat: redact implementation and custom validation for the fields

--- mvc-example/models/comment.js
+++ mvc-example/models/comment.js
@@ -1,10 +1,17 @@
 const { Schema, model } = require('mongoose');
+const Redact = require('./redact');
 
 const commentSchema = new Schema({
   description: {
     type: String,
-    required: true,
-    minLength: 2,
+    required: [true, 'A descricao e obrigatoria'],
+    minLength: [2, 'O comentario precisa ter pelo menos 2 caracteres'],
+    validate: {
+      validator: (value) => Redact
+        .count({ term: value })
+        .then((count) => count === 0),
+      message: 'A palavra {VALUE} nao pode ser usada',
+    },
   },
   post: {
     type: Schema.Types.ObjectId,
--- mvc-example/models/post.js
+++ mvc-example/models/post.js
@@ -1,15 +1,28 @@
 const { Schema, model } = require('mongoose');
+const Redact = require('./redact');
 
 const postSchema = new Schema({
   title: {
     type: String,
-    required: true,
-    minLength: 2,
+    required: [true, 'O titulo e obrigatorio'],
+    minLength: [2, 'O titulo precisa ter pelo menos 2 caracteres'],
+    validate: {
+      validator: (value) => Redact
+        .count({ term: value })
+        .then((count) => count === 0),
+      message: 'A palavra {VALUE} nao pode ser usada no titulo',
+    },
   },
   description: {
     type: String,
-    required: true,
-    minLength: 2,
+    required: [true, 'A descricao e obrigatoria'],
+    minLength: [2, 'a descricao precisa ter pelo menos 2 caracteres'],
+    validate: {
+      validator: (value) => Redact
+        .count({ term: value })
+        .then((count) => count === 0),
+      message: 'A palavra {VALUE} nao pode ser usada na descricao',
+    },
   },
   comments: [{
     type: Schema.Types.ObjectId,
