4dcca9602874e5803e18241af0c45465c3dd9397
SamuelFST <guti.samuel2002@gmail.com>
2022-07-12 23:22:08 -0300

    feat: api basic configs

---
+++ social-network_api/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    'class-methods-use-this': 0,
+    'import/first': 0,
+    'arrow-body-style': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    camelcase: 0,
+    'no-unused-vars': 0,
+  },
+};
