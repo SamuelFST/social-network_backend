fcff761675c4068a167618edbacedd08f6a0a888
SamuelFST <guti.samuel2002@gmail.com>
2022-07-16 21:23:20 -0300

    chore: removed routes

--- social-network_api/routers/user.js
+++ social-network_api/routers/user.js
@@ -1,12 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 const { User, Connection } = require('../models');
-
-const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'someSuperSecretToken123';
 
 router
   .route('/')
@@ -15,74 +11,51 @@ router
     .then(() => {
       next();
     })
-    .catch((err) => next(err)))
-
-/**
- * @route GET /users
- * @group User - api
- * @returns {User} 200 - Array of user info
- * @returns {Error} default - unexpected error
- * @security JWT
- */
-  .get((req, res, next) => Promise.resolve()
-    .then(() => User.find({}))
-    .then((data) => res.status(200).json(data))
-    .catch((err) => next(err)))
-
-/**
- * @route POST /users
- * @param {User.model} post.body.required - new user
- * @group User - api
- */
-  .post((req, res, next) => Promise.resolve()
-    .then(() => bcrypt.hash(req.body.password, 10))
-    .then((passwordHashed) => new User({ ...req.body, password: passwordHashed }).save())
-    .then((data) => res.status(201).json(data))
-    .catch((err) => next(err)));
-
-/**
- * Login
- * @route POST /users/login
- * @param {User.model} post.body.required - user and password
- * @group User - api
- */
-router
-  .route('/login')
-  .post((req, res, next) => Promise.resolve()
-    .then(() => User.findOne({ user: req.body.user }))
-    .then((user) => (user
-      ? bcrypt.compare(req.body.password, user.password)
-      : next(createError(404))))
-    .then((passwordHashed) => (passwordHashed
-      ? jwt.sign(req.body.user, accessTokenSecret)
-      : next(createError(401))))
-    .then((token) => res.status(201).json({ msg: 'User authenticated!', token }))
     .catch((err) => next(err)));
 
 router
   .param('id', (req, res, next, id) => Promise.resolve()
     .then(() => Connection.then())
-    .then(() => {
-      // console.log(`Request from : ${req.originalUrl}`);
-      // console.log(`Request type : ${req.method}`);
-      // console.log(`Request id : ${id}`);
-      next();
-    })
+    .then(() => next())
     .catch((err) => next(err)))
-  .route('/:id')
+  .route('/me')
 
+/**
+ * Get my user
+ * @route GET /users/me
+ * @group User - api
+ * @returns {User} 200 - my user
+ * @returns {Error} default - unexpected error
+ * @security JWT
+ */
   .get((req, res, next) => Promise.resolve()
-    .then(() => User.findById(req.params.id).populate({ path: 'comments' }))
+    .then(() => User.findById(req.user.id))
     .then((data) => (data ? res.status(200).json(data) : next(createError(404))))
     .catch((err) => next(err)))
 
+/**
+ * Update my user
+ * @route PUT /users/me
+ * @group User - api
+ * @returns {User} 200 - my user
+ * @returns {Error} default - unexpected error
+ * @security JWT
+ */
   .put((req, res, next) => Promise.resolve()
-    .then(() => User.findByIdAndUpdate(req.params.id, req.body, { runValidators: true }))
+    .then(() => User.findByIdAndUpdate(req.user.id, req.body, { runValidators: true }))
     .then((data) => res.status(203).json(data))
     .catch((err) => next(err)))
 
+/**
+ * Delete my user
+ * @route DELETE /users/me
+ * @group User - api
+ * @returns {User} 200 - my user
+ * @returns {Error} default - unexpected error
+ * @security JWT
+ */
   .delete((req, res, next) => Promise.resolve()
-    .then(() => User.deleteOne({ _id: req.params.id }))
+    .then(() => User.deleteOne({ _id: req.user.id }))
     .then((data) => res.status(203).json(data))
     .catch((err) => next(err)));
 
