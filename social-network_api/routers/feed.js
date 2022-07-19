fb541da6872224e8ea1c9460f3814a77b23115ab
SamuelFST <guti.samuel2002@gmail.com>
2022-07-19 20:19:33 -0300

    chore: pagination for posts feed

--- social-network_api/routers/feed.js
+++ social-network_api/routers/feed.js
@@ -13,15 +13,21 @@ router
 
 /**
  * Get feed of posts
- * @route GET /feed
+ * @route GET /feed?page={page}
+ * @param {integer} page.query - current page
  * @group Feed - api
  * @returns {Array.<Post>} 200 - Array of posts
  * @returns {Error} default - unexpected error
  * @security JWT
  */
   .get((req, res, next) => Promise.resolve()
-    .then(() => Profile.findById(req.user.profile.id))
-    .then((profile) => Post.find({ profile: { $in: profile.following } }).populate('profile'))
+    .then(() => Profile.findById(req.user.profile._id))
+    .then((profile) => Post
+      .find({ profile: { $in: [...profile.following, req.user.profile._id] } })
+      .populate('profile')
+      .limit(10)
+      .skip((req.query.page || 0) * 10)
+      .sort({ createdAt: 'desc' }))
     .then((data) => res.status(200).json(data))
     .catch((err) => next(err)));
 
